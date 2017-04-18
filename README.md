9881627a636295c324a0aefbb12ac90278eeba2d
Your Name <you@example.com>
2017-04-18 02:59:32 -0700

    Added test_hello1.py

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # testx_repo
 Test Repo to learn git basics
+
+
+adding hello file was the change
--- test_hello.py
+++ test_hello.py
@@ -1,1 +1,5 @@
-print "Hello Sindhu"
+<<<<<<< HEAD
+print "Hello world"
+=======
+print "Hello Kalai"
+>>>>>>> e5854e9e59c07b67fc61ffe88b037fd8d5b7e4cf
